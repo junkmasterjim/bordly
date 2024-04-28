e1c337fc4d08e234e5091bc880a415158d154723
Noah Pittman <noahpittman00@gmail.com>
2024-04-28 18:48:07 -0400

    remove sidebar on mobile

--- components/app-layout/sidebar.tsx
+++ components/app-layout/sidebar.tsx
@@ -21,7 +21,7 @@ export const Sidebar = ({
 
 	return (
 		<>
-			<div className="relative">
+			<div className="relative hidden sm:block">
 				<motion.button
 					animate={{
 						display: isOpen ? "none" : "flex",
@@ -34,48 +34,51 @@ export const Sidebar = ({
 					<ChevronsRight className="h-4 w-4" />
 				</motion.button>
 			</div>
-			<motion.div
-				style={{
-					width: isOpen ? "250px" : "0",
-					overflowX: isOpen ? "visible" : "hidden",
-				}}
-				animate={{
-					width: isOpen ? "250px" : "0",
-					overflowX: isOpen ? "visible" : "hidden",
-				}}
-				className="w-full max-w-[250px] h-full border-r text-foreground py-2 relative"
-			>
-				<div className="h-8 absolute top-2 right-2 z-50">
-					<button
-						className="hover:bg-accent p-1 rounded"
-						onClick={() => setIsOpen((prev) => !prev)}
-					>
-						<ChevronsLeft className="h-4 w-4" />
-					</button>
-				</div>
-				<div className="container flex flex-col gap-4 items-center truncate pt-8">
-					<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
-						Text Content <ArrowRight className="h-4 w-4" />
-						<HeadingsDropdown brand={brand} setBrand={setBrand} />
+
+			<div className="hidden sm:block">
+				<motion.div
+					style={{
+						width: isOpen ? "250px" : "0",
+						overflowX: isOpen ? "visible" : "hidden",
+					}}
+					animate={{
+						width: isOpen ? "250px" : "0",
+						overflowX: isOpen ? "visible" : "hidden",
+					}}
+					className="w-full max-w-[250px] h-full border-r text-foreground py-2 relative"
+				>
+					<div className="h-8 absolute top-2 right-2 z-50">
+						<button
+							className="hover:bg-accent p-1 rounded"
+							onClick={() => setIsOpen((prev) => !prev)}
+						>
+							<ChevronsLeft className="h-4 w-4" />
+						</button>
 					</div>
-					<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
-						Font Family <ArrowRight className="h-4 w-4" />
-						<FontsDropdown brand={brand} setBrand={setBrand} />
+					<div className="container flex flex-col gap-4 items-center truncate pt-8">
+						<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
+							Text Content <ArrowRight className="h-4 w-4" />
+							<HeadingsDropdown brand={brand} setBrand={setBrand} />
+						</div>
+						<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
+							Font Family <ArrowRight className="h-4 w-4" />
+							<FontsDropdown brand={brand} setBrand={setBrand} />
+						</div>
+						<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
+							Font Weight <ArrowRight className="h-4 w-4" />
+							<WeightsDropdown brand={brand} setBrand={setBrand} />
+						</div>
+						<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
+							Trademark <ArrowRight className="h-4 w-4" />
+							<TMToggle brand={brand} setBrand={setBrand} />
+						</div>
+						<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
+							Underline <ArrowRight className="h-4 w-4" />
+							<UnderlineToggle brand={brand} setBrand={setBrand} />
+						</div>
 					</div>
-					<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
-						Font Weight <ArrowRight className="h-4 w-4" />
-						<WeightsDropdown brand={brand} setBrand={setBrand} />
-					</div>
-					<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
-						Trademark <ArrowRight className="h-4 w-4" />
-						<TMToggle brand={brand} setBrand={setBrand} />
-					</div>
-					<div className="flex items-center justify-between text-sm font-semibold select-none gap-2 w-full">
-						Underline <ArrowRight className="h-4 w-4" />
-						<UnderlineToggle brand={brand} setBrand={setBrand} />
-					</div>
-				</div>
-			</motion.div>
+				</motion.div>
+			</div>
 		</>
 	);
 };
